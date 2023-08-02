48fb762d62d7b0e3b147dea8f0155ef19f5865d4
UnaSombraaa <127241057+UnaSombraaa@users.noreply.github.com>
2023-08-02 18:01:16 -0300

    conf css
    u

--- Creat3D/Codigo/js/Configuracion.js
+++ Creat3D/Codigo/js/Configuracion.js
@@ -1,63 +1,40 @@
 document.getElementById("accountInfoButton").addEventListener("click", function() {
-    showSection("accountInfoSection");
-  });
-  
-  document.getElementById("privacyButton").addEventListener("click", function() {
-    showSection("privacySection");
-  });
-  
-  document.getElementById("transactionsButton").addEventListener("click", function() {
-    showSection("transactionsSection");
-  });
-  
-  document.getElementById("accountInfoForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    // Lógica para guardar los cambios en la información de la cuenta
-  });
-  
-  document.getElementById("privacyForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    // Lógica para guardar los cambios en la privacidad y seguridad
-  });
-  
-  document.getElementById("deleteAccountButton").addEventListener("click", function() {
-    // Lógica para eliminar la cuenta del usuario
-  });
-  
-  document.getElementById("logoutButton").addEventListener("click", function() {
-    // Lógica para cerrar sesión
-  });
-  
-  // Función para mostrar la sección deseada y ocultar las demás
-  function showSection(sectionId) {
-    var sections = document.getElementsByClassName("content")[0].children;
-    for (var i = 0; i < sections.length; i++) {
-      sections[i].style.display = "none";
-    }
-    document.getElementById(sectionId).style.display = "block";
+  showContent("accountInfo");
+});
+
+document.getElementById("privacySecurityButton").addEventListener("click", function() {
+  showContent("privacySecurity");
+});
+
+document.getElementById("transactionsDonationsButton").addEventListener("click", function() {
+  showContent("transactionsDonations");
+});
+
+document.getElementById("accessibilityButton").addEventListener("click", function() {
+  showContent("accessibility");
+});
+
+document.getElementById("darkModeButton").addEventListener("click", function() {
+  // Lógica para cambiar a modo oscuro
+});
+
+function showContent(contentId) {
+  var contents = document.getElementsByClassName("content");
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = "none";
   }
-  
-  // Ejemplo de cómo mostrar compras y donaciones
-  var purchasesList = document.getElementById("purchasesList");
-  var donationsList = document.getElementById("donationsList");
-  
-  // Función para agregar una compra a la lista
-  function addPurchase(productName, price) {
-    var li = document.createElement("li");
-    li.textContent = "Compra: " + productName + " - $" + price;
-    purchasesList.appendChild(li);
-  }
-  
-  // Función para agregar una donación a la lista
-  function addDonation(donationName, amount) {
-    var li = document.createElement("li");
-    li.textContent = "Donación: " + donationName + " - $" + amount;
-    donationsList.appendChild(li);
-  }
-  
-  // Ejemplo de uso de las funciones addPurchase y addDonation
-  addPurchase("Producto 1", 20);
-  addPurchase("Producto 2", 30);
-  addDonation("Donación 1", 10);
-  addDonation("Donación 2", 15);
-  
+  document.getElementById(contentId).style.display = "block";
+}
+
+// Aquí puedes implementar la lógica para mostrar el historial de compras y donaciones
+
+// Ejemplo de lógica para guardar los cambios en la información de la cuenta
+document.getElementById("accountInfo").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var phoneNumber = document.getElementById("phoneNumber").value;
+
+  // Lógica para guardar los cambios en la información de la cuenta
+});
